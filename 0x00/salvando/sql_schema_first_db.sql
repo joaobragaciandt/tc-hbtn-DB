ca89e8e3099172ed8068235b0dde6e9f40ab9ecf
joaobragaciandt <joaobraga@ciandt.com>
2022-07-20 14:20:25 -0300

    4. Salvando o SQL Schema - SQLite

---
+++ 0x00/salvando/sql_schema_first_db.sql
@@ -0,0 +1,14 @@
+-- TABLE
+CREATE TABLE Cliente (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+nome TEXT NOT NULL,
+email TEXT NOT NULL,
+telefone INTEGER);
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
